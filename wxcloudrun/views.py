b74336b3cbf771050ae396ca3c0f4c765657e295
Zhang-congzhi <3235730264@qq.com>
2022-03-21 23:42:35 +0800

    实现读取图片的url

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -7,7 +7,7 @@ from wxcloudrun.models import Counters
 
 import requests
 
-from skimage import io
+# from skimage import io
 
 logger = logging.getLogger('log')
 
@@ -46,8 +46,8 @@ def zcztest(request, _):
     r = requests.post(url, data=data, headers=headers)
     image_url = r.json()['file_list'][0]['download_url']
     
-    image = io.imread(image_url)
-    image_shape = str(image.shape)
+    # image = io.imread(image_url)
+    # image_shape = str(image.shape)
 
 
     rsp = JsonResponse({'code': 0, 'errorMsg': ''}, json_dumps_params={'ensure_ascii': False})
@@ -59,7 +59,7 @@ def zcztest(request, _):
             "data": 'FengFu success!',
             'message':'我已经成功获得图片url!',
             'url':image_url,
-            '图片大小':image_shape},
+            },
                     json_dumps_params={'ensure_ascii': False})
     else:
         rsp = JsonResponse({'code': -1, 'errorMsg': '请求方式错误'},

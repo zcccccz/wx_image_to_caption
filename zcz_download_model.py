446594d040dde617fba019c2f190998e219051af
zcz <3235730264@qq.com>
2022-03-24 12:58:31 +0800

    添加神经网络，从数据库读取模型

--- zcz_download_model.py
+++ zcz_download_model.py
@@ -5,9 +5,7 @@ import sys
 import logging
 import requests
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-
 r = requests.get('http://api.weixin.qq.com/_/cos/getauth')
-
 secret_id = r.json()["TmpSecretId"]
 secret_key = r.json()["TmpSecretKey"]   
 region = 'ap-shanghai'      
@@ -18,7 +16,7 @@ client = CosS3Client(config)
 response = client.download_file(
     Bucket='7072-prod-1gxq0vrt5f11b56e-1310101813',
     Key='/models/bua-d2-frcn-r101.pth',
-    DestFilePath='bottom_up_attention/bua-d2-frcn-r101.pth'
+    DestFilePath='bottom-up-attention.pytorch/bua-d2-frcn-r101.pth'
 )
 response = client.download_file(
     Bucket='7072-prod-1gxq0vrt5f11b56e-1310101813',
